f6af79f6834f5c00472af08905e7129f4f66df37
Professional <k_groznov@mail.ru>
2022-06-04 16:08:58 +0300

    Практика 10

--- praktika10/App.test.js
+++ praktika10/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../praktika8/App';
+import App from './App';
 
 test('renders learn react link', () => {
     render(<App />);
--- praktika10/index.js
+++ praktika10/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import {Asteroids} from './pages/asteroids';
-import {Destroy} from './pages/destroy';
+import {App} from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Asteroids />}/>
-                <Route path="/destroy" element={<Destroy />}/>
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>
 );
 
